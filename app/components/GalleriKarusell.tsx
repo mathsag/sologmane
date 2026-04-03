088367514ce35ec2376cb1b176848fdb33aee477
mathsag <mathias.sagen1@gmail.com>
2026-04-03 10:31:07 +0200

    Duplication only with 3+ images

--- app/components/GalleriKarusell.tsx
+++ app/components/GalleriKarusell.tsx
@@ -1,11 +1,11 @@
 import type { GalleriBilde } from '@/lib/types'
 
 export default function GalleriKarusell({ bilder }: { bilder: GalleriBilde[] }) {
-  const dobbel = [...bilder, ...bilder]
+  const visning = bilder.length >= 3 ? [...bilder, ...bilder] : bilder
   return (
     <div className="galleri-track-wrap">
       <div className="galleri-track">
-        {dobbel.map((b, i) => (
+        {visning.map((b, i) => (
           <div key={i} className="galleri-bilde">
             <img src={b.url} alt={`Bilde ${i + 1}`} />
           </div>
